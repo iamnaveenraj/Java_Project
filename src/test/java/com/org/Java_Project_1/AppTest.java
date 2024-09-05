81a5f3b3113c3b422ff66c016384b3a8e76abfdd
Naveen Kumar <naveenraj100198@gmail.com>
2024-09-05 19:43:20 +0530

    Files Added

---
+++ src/test/java/com/org/Java_Project_1/AppTest.java
@@ -0,0 +1,19 @@
+package com.org.Java_Project_1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
