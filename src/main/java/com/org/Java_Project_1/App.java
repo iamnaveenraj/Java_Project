f68d689c83c83659e80479146c513ffd654a29a2
Naveen Kumar <55154827+iamnaveenraj@users.noreply.github.com>
2024-09-06 22:00:34 +0530

    Adding duplicay

--- src/main/java/com/org/Java_Project_1/App.java
+++ src/main/java/com/org/Java_Project_1/App.java
@@ -4,5 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello DevOps!");
         System.out.println("New Line Added");
+        System.out.println("Hello DevOps!");
+        System.out.println("New Line Added");
+        System.out.println("Hello DevOps!");
+        System.out.println("New Line Added");
     }
 }
